9dd48a1cb4511d30ebe78fee068823b5a8886c70
Mohammed Nabil <Nabil1243@outlook.com>
2024-06-19 06:52:55 -0700

    added checkout flow

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoutes from "./auth/ProtectedRoutes";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 const AppRoutes = () => {
   return (
     <Routes>
@@ -23,6 +24,14 @@ const AppRoutes = () => {
         element={
           <Layout showHero={false}>
             <SearchPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
           </Layout>
         }
       />
